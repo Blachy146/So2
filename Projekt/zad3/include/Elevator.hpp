cd267dad9b44cf3afbe4d784c88d7fe7f140197b
bmalecki <blachypk@gmail.com>
2018-05-26 16:42:41 +0200

    .

--- Projekt/zad3/include/Elevator.hpp
+++ Projekt/zad3/include/Elevator.hpp
@@ -1,10 +1,20 @@
 #ifndef ELEVATOR_HPP
 #define ELEVATOR_HPP
 
+#include <memory>
+#include <list>
+
+#include <Person.hpp>
+
 
 class Elevator
 {
-
+public:
+    explicit Elevator(const int capacity);
+    ~Elevator();
+private:
+    const int capacity;
+    std::list<std::shared_ptr<Person>> people;
 };
 
 #endif // ELEVATOR_HPP
--- Projekt/zad3/source/Elevator.cpp
+++ Projekt/zad3/source/Elevator.cpp
@@ -1,1 +1,15 @@
+#include <memory>
+#include <list>
+
 #include <Elevator.hpp>
+#include <Person.hpp>
+
+
+Elevator::Elevator(const int capacity)
+    : capacity(capacity)
+{
+}
+
+Elevator::~Elevator()
+{
+}
